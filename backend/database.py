131ee5eb649b1b4295d2f7f911e02c6df454c4f6
morf1sm <voroninn889@gmail.com>
2026-07-01 19:45:42 +0300

    second

--- backend/database.py
+++ backend/database.py
@@ -1,16 +1,23 @@
 import asyncpg
+import os
 
 pool = None
 
+
 async def init_db():
     global pool
-    pool = await asyncpg.create_pool(
-        user="semenbarinov",
-        password="semenbarinov",
-        database="visi_db",
-        host="127.0.0.1",
-        port=5432
-    )
+    db_url = os.environ.get("DATABASE_URL")
+
+    if db_url:
+        pool = await asyncpg.create_pool(db_url)
+    else:
+        pool = await asyncpg.create_pool(
+            user="semenbarinov",
+            password="semenbarinov",
+            database="visi_db",
+            host="127.0.0.1",
+            port=5432
+        )
 
 
 async def close_db():
